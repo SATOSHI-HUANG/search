caa0a1bbc33a5fc3e87a7c6982dace5b13031b25
SATOSHI-HUANG <coming0302@gmail.com>
2020-02-28 22:38:20 +0800

    改變首頁路徑

--- src/router.js
+++ src/router.js
@@ -14,7 +14,7 @@ export default new Router({
     mode: 'history',
     routes:[
         {
-            path: '/',
+            path: '/search',
             name: 'list',
             component: List
         },
